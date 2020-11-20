b8867fba6215d280b4eb659744736ebb29673789
DISCORDBBBB <60780527+BSGreatuser@users.noreply.github.com>
2020-11-20 21:18:39 +0900

    Add files via upload

---
+++ del_log.py
@@ -0,0 +1,37 @@
+
+###################
+####python 3.x#####
+#discord.py==1.4.0#
+###################
+
+import discord
+import asyncio
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("봇이 성공적으로 실행되었습니다.")
+    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message_delete(message):
+    y = datetime.datetime.now().year
+    m = datetime.datetime.now().month
+    d = datetime.datetime.now().day
+    h = datetime.datetime.now().hour
+    min = datetime.datetime.now().minute
+    bot_logs = '★로그채널 ID★'
+    embed = discord.Embed(title='메시지 삭제됨', colour=discord.Colour.orange())
+    embed.add_field(name='유저', value=f'<@{message.author.id}>({message.author})')
+    embed.add_field(name='채널', value=f'<#{message.channel.id}>')
+    embed.add_field(name='내용', value=message.content, inline=False)
+    embed.add_field(name='날짜', value=f"{y}-{m}-{d} {h}:{min}", inline=False)
+    embed.set_footer(text=f"유저 ID:{message.author.id} • 메시지 ID: {message.id}")
+    await client.get_channel(int(bot_logs)).send(embed=embed)
+
+
+client.run('★TOKEN★')
